754516368a0b1546b0cd6f88fe72e007f8c38ffb
DriftingPixel <definitlyplayer1@gmail.com>
2025-11-21 22:58:46 +0530

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -794,7 +794,9 @@ export default function Home() {
       <div className="image-viewer-overlay"></div>
 
       <div className="image-viewer-panel">
-        <button className="image-viewer-close">×</button>
+        <button className="image-viewer-close">
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
+        </button>
         <img className="image-viewer-img" src="" alt="Enlarged view" />
       </div>
 
