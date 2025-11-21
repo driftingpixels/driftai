0b141535d5adc6b4ac90acf21bf13c1f6b492c3a
DriftingPixel <definitlyplayer1@gmail.com>
2025-11-21 22:54:50 +0530

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -299,7 +299,7 @@ export default function Home() {
 
       const removeBtn = document.createElement("button");
       removeBtn.className = "remove-image-btn";
-      removeBtn.innerHTML = "×";
+      removeBtn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>';
       removeBtn.onclick = () => {
         selectedImages = selectedImages.filter(img => img.data !== imageData.data);
         previewDiv.remove();
